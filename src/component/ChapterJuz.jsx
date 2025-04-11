6e142ee646e8a9390d7f01d91e9ca6eb3391bdd8
amdslhn <ahmadsholihin.smp.it@gmail.com>
2025-04-11 15:22:53 +0700

    Change Spinner

--- src/component/ChapterJuz.jsx
+++ src/component/ChapterJuz.jsx
@@ -4,7 +4,7 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const Spinner = () => (
   <div className="flex justify-center items-center py-10">
-    <div className="w-10 h-10 border-4 border-blue-500 border-dotted rounded-full animate-spin"></div>
+    <div className="w-10 h-10 border-4 border-black border-dotted rounded-full animate-spin"></div>
   </div>
 );
 
