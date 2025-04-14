f8fa6d0d3675564b4afb0dcae33ba2eda81b74e9
amdslhn <ahmadsholihin.smp.it@gmail.com>
2025-04-14 10:05:23 +0700

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
