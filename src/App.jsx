94a229f983f62c610e8cc7285aa6a6218d4d0c55
amdslhn <ahmadsholihin.smp.it@gmail.com>
2025-04-14 10:22:08 +0700

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 
 import PerAyat from "./Pages/PerAyat";
 import Perjuz from "./Pages/PerJuz";
