eadd1ad286ef932266293fc0fae246b7abfac2f5
amdslhn <ahmadsholihin.smp.it@gmail.com>
2025-04-14 10:08:33 +0700

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,12 @@ import List from "./Pages/List";
 
 function App() {
     return (
-        <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/list" element={<List />} />
             <Route path="/by_surah/:id" element={<PerAyat />} />
             <Route path ="/by_juz/:id" element={<Perjuz />} />
         </Routes>
-        </BrowserRouter>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import { StrictMode } from 'react'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
